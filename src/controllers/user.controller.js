ff6843ce3ffd1a6a26da15cfd87531dae3187312
Gustavo Age <ageg590@gmail.com>
2025-10-21 23:08:39 -0300

    refactor: Connected to MongoDB Atlas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,18 @@
+const userService = require('../services/user.service');
 
-const create = (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body;
+const create = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({message: 'All fields are required'});
+        res.status(400).send({ message: 'All fields are required' });
     }
-    res.status(201).send({message: 'User created successfully'});
-}
+
+    const user = await userService.create(req.body);
+
+    if (!user) {
+        return res.status(400).send({ message: 'Error creating user' });
+    }
+    res.status(201).send({ message: 'User created successfully' });
+};
 
 module.exports = { create };
---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    console.log('Connecting to database...');
+
+    mongoose.connect(
+        'mongodb+srv://gusage:gusage@cluster0.i2hjedu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Database connected successfully'))
+    .catch((err) => console.log('Database connection error:', err));
+};
+
+module.exports = connectDatabase;
