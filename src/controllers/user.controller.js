00c228911d9903f0cb71af9f32779ddba6a59e11
Gustavo Age <ageg590@gmail.com>
2025-10-22 13:43:30 -0300

    refactor: adicioned a find users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const { find } = require('../models/User');
 const userService = require('../services/user.service');
 
 const create = async (req, res) => {
@@ -7,7 +9,7 @@ const create = async (req, res) => {
         res.status(400).send({ message: 'All fields are required' });
     }
 
-    const user = await userService.create(req.body);
+    const user = await userService.createService(req.body);
 
     if (!user) {
         return res.status(400).send({ message: 'Error creating user' });
@@ -15,4 +17,27 @@ const create = async (req, res) => {
     res.status(201).send({ message: 'User created successfully' });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (users === 0) {
+        return res.status(400).send({ message: 'No users found' });
+    }
+
+    res.send(users);
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send({ message: 'Invalid ID' });
+    }
+
+    const user = await userService.findByIdService(id)
+    if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+    }
+    res.send(user);
+}
+module.exports = { create, findAll, findById };
