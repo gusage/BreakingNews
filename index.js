c9749f2492a6a3307dd2adf6ac9a90fb672f8419
Gustavo Age <ageg590@gmail.com>
2025-10-24 19:07:26 -0300

    feat: added create News

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import connectDatabase from './src/database/db.js';
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 
+import newsRoute from './src/routes/news.route.js';
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -12,7 +14,8 @@ const app = express()
 
 connectDatabase();
 app.use(express.json());
-app.use('/user.js', userRoute);
-app.use('/auth.js', authRoute);
+app.use('/user', userRoute);
+app.use('/auth', authRoute);
+app.use('/news', newsRoute);
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
---
+++ src/controllers/news.controller.js
@@ -0,0 +1,31 @@
+import { createService, findAllService } from '../services/news.service.js';
+
+const create = async (req, res) => {
+    
+    try {
+        const {title, content, bannerImage} = req.body;
+
+        if (!title || !content || !bannerImage) {
+            return res.status(400).send({ message: 'Title, content and bannerImage are required.' });
+        }
+
+        await createService({
+            title,
+            content,
+            bannerImage,
+            user: 'objectId01'
+        })
+
+        res.send(201);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+    
+}
+
+const findAll = (req, res) => {
+    const news = [];
+    res.send(news);
+}
+
+export default { create, findAll };
