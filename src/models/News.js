0204945e58bef9fe6097ff8d086583b5b7df2829
Gustavo Age <ageg590@gmail.com>
2025-10-24 18:49:33 -0300

    feat: create News schema

---
+++ src/models/News.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    bannerImage: {
+        type: String,
+        required: false,
+    },
+    publishedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    likes: {
+        type: Array,
+        required: true,
+    },
+    comments: {
+        type: Array,
+        required: true,
+    }
+})
+
+const News = mongoose.model('News', newsSchema);
+
+export default News;
